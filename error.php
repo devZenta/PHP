0937f93461263e9e2803b883fd4be9fa5b304f5a
Hugo Ghesquier <hugo.ghesquier59@gmail.com>
2025-09-30 14:31:05 +0200

    feat: add error file

---
+++ error.php
@@ -0,0 +1,11 @@
+<?php
+
+// ! Affichage des erreurs
+// ! Ne pas mettre ce code en PROD
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// ! Code pour l'erreur
+echo $error;
+?>
