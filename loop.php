94da648dc867222421c12afc8a9445e528b6588f
Hugo Ghesquier <hugo.ghesquier59@gmail.com>
2025-09-30 14:36:21 +0200

    feat: add loop functionality to display counter and numbers

---
+++ loop.php
@@ -0,0 +1,14 @@
+<?php 
+
+    for($i = 1; $i <= 5; $i++) {
+        echo "Counter : $i <br>";
+    }
+
+    $number = 0;
+
+    while ($number <= 10) {
+        echo "Number : $number <br>";
+        $number ++;
+    }
+
+?>
