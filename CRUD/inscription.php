b36fc30be740885bfda2f01aa69a5039504d6aeb
Hugo Ghesquier <hugo.ghesquier59@gmail.com>
2025-10-03 12:36:30 +0200

    feat: add user registration form with input fields for name, email, and password

---
+++ CRUD/inscription.php
@@ -0,0 +1,6 @@
+<form method="POST" action="createUser.php">
+    Name: <input type="text" name="name">
+    Email: <input type="text" name="email">
+    Password: <input type="password" name="password"><br><br>
+    <input type="submit" value="Créer mon compte">
+</form>
