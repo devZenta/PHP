4f6cec244c8bffe6923f378967f1c0b3fab9e67e
Hugo Ghesquier <hugo.ghesquier59@gmail.com>
2025-10-06 14:23:56 +0200

    feat: add user management forms for account creation, modification, deletion, and user display

---
+++ CRUD/panel.php
@@ -0,0 +1,15 @@
+<form method="POST" action="inscription.php">
+    <input type="submit" value="Créer mon compte">
+</form>
+
+<form method="POST" action="editUserForm.php">
+    <input type="submit" value="Modification de mon compte">
+</form>
+
+<form method="POST" action="deleteUserForm.php">
+    <input type="submit" value="Supprimer mon compte">
+</form>
+
+<form method="POST" action="getAllUsers.php">
+    <input type="submit" value="Affichage des utilisateurs">
+</form>
